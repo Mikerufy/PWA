d7c632721c3593d49be270cf8a166f98068b8b55
Mikerufy <smartsanjeevan@gmail.com>
2022-05-05 00:10:21 +0530

    PWA

--- pwa-tutorial/sw.js
+++ pwa-tutorial/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v2';
-const dynamicCacheName = 'site-dynamic-v1';
+// const staticCacheName = 'site-static-v2';
+// const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
   '/',
   '/index.html',
@@ -14,46 +14,71 @@ const assets = [
   '/pages/fallback.html'
 ];
 
-// install event
-self.addEventListener('install', evt => {
-  //console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
+// // install event
+// self.addEventListener('install', evt => {
+//   //console.log('service worker installed');
+//   evt.waitUntil(
+//     caches.open(staticCacheName).then((cache) => {
+//       console.log('caching shell assets');
+//       cache.addAll(assets);
+//     })
+//   );
+// });
+
+// // activate event
+// self.addEventListener('activate', evt => {
+//   //console.log('service worker activated');
+//   evt.waitUntil(
+//     caches.keys().then(keys => {
+//       //console.log(keys);
+//       return Promise.all(keys
+//         .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+//         .map(key => caches.delete(key))
+//       );
+//     })
+//   );
+// });
+
+// // fetch event
+// self.addEventListener('fetch', evt => {
+//   //console.log('fetch event', evt);
+//   evt.respondWith(
+//     caches.match(evt.request).then(cacheRes => {
+//       return cacheRes || fetch(evt.request).then(fetchRes => {
+//         return caches.open(dynamicCacheName).then(cache => {
+//           cache.put(evt.request.url, fetchRes.clone());
+//           return fetchRes;
+//         })
+//       });
+//     }).catch(() => {
+//       if(evt.request.url.indexOf('.html') > -1){
+//         return caches.match('/pages/fallback.html');
+//       } 
+//     })
+//   );
+// });
+
+if ('serviceWorker' in navigator){
+  window.addEventListener('load',function()
+  {
+      this.navigator.serviceWorker.register('./sw.js')
+      .then((registration) => {console.log("hi")})
+      .catch(function(e){console.log(e)})
+  })
+}
+
+self.addEventListener('fetch',function(event){
+  console.log(event.request.url)
+})
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('cacheName').then(function(cache) {
+      return cache.addAll(assets);
     })
   );
 });
 
-// activate event
-self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      //console.log(keys);
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// fetch event
-self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          return fetchRes;
-        })
-      });
-    }).catch(() => {
-      if(evt.request.url.indexOf('.html') > -1){
-        return caches.match('/pages/fallback.html');
-      } 
-    })
-  );
-});
+// Notification.requestPermission(function(status) {
+//   console.log('Notification permission status:', status);
+// });
